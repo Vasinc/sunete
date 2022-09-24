2dddab56157863e28acbcfe58c4f906d9210ce17
Vasinc <lein0.jade@gmail.com>
2022-09-24 18:57:55 +0300

    3 ( upper section added )

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,9 @@
-console.log('test');
+const skull = document.getElementById('skull');
+
+window.addEventListener('scroll', () => {
+    const scrollY = Math.trunc(window.scrollY / 10);
+
+    skull.style.transform = `translate(-50%, calc(-50% + ${scrollY}%))`
+
+    console.log(scrollY)
+})
