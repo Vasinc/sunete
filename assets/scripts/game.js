2d83e2142bad4468afecf43a4e31123fe3cdb7ee
Vasinc <lein0.jade@gmail.com>
2022-09-25 22:29:52 +0300

    7 ( game gameplay )

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -11,6 +11,14 @@ const fame = document.querySelector('.fame');
 const singingMoney = [1, 50];
 const concertMoney = [5000, 10000];
 
+// screen.orientation.lock("portrait")
+// 	.then(function() {
+// 		alert('Locked');
+// 	})
+// 	.catch(function(error) {
+// 		alert(error);
+// 	});
+
 const audio = new Audio('./assets/sounds/audio.mp3');
 audio.volume = .3;
 const crowd = new Audio('./assets/sounds/crowd.mp3');
@@ -18,6 +26,8 @@ crowd.volume = .3;
 
 let MONEY_VAL = 0;
 let FAME_VAL = 0;
+let fameMultiplier = 1;
+let concertMultiplier = 500;
 
 let isSinging = false;
 
@@ -31,9 +41,24 @@ function updateMoney () {
     money.textContent = MONEY_VAL;
 }
 
+updateMoney();
+
 function updateFame () {
     fame.textContent = FAME_VAL;
 }
+
+function updateStats() {
+    document.querySelector('.stat__min-money_sing').textContent = singingMoney[0];
+    document.querySelector('.stat__max-money_sing').textContent = singingMoney[1];
+    
+    document.querySelector('.stat__min-money_concert').textContent = concertMoney[0];
+    document.querySelector('.stat__max-money_concert').textContent = concertMoney[1];
+
+    document.querySelector('.stat__fame-sign').textContent = fameMultiplier * 1;
+    document.querySelector('.stat__fame-concert').textContent = concertMultiplier;
+}
+
+updateStats();
 
 function sing () {
     const rndNum = Math.trunc(Math.random() * (singingMoney[1] - singingMoney[0]) + singingMoney[0])
@@ -44,7 +69,7 @@ function sing () {
         audio.currentTime = 0;
         MONEY_VAL += rndNum;
         updateMoney();
-        FAME_VAL += 1;
+        FAME_VAL += 1 * fameMultiplier;
         updateFame();
         isSinging = false;
         bvcovia.style.cursor = 'pointer';
@@ -52,9 +77,9 @@ function sing () {
         bvcoviaText.classList.add('display-block');
         setTimeout(() => {
             bvcoviaText.classList.remove('display-block');
-        }, 300)
+        }, 2000)
     },
-    500
+    4500
     )
 }
 
@@ -69,9 +94,44 @@ bvcovia.addEventListener('click', () => {
 
 backdrop.addEventListener('click', toggleBackdrop);
 
-shopBtn.addEventListener('click', () => {
+shopBtn.addEventListener('click', (event) => {
     toggleBackdrop();
     shopUI.classList.add('display-block');
+})
+
+shopUI.addEventListener('click', (event) => {
+    if (event.target.tagName == 'BUTTON') {
+        let price = parseInt(event.target.textContent);
+        let minLevel = parseInt(event.target.parentElement.querySelector('.min-level').textContent.trim());
+        const maxLevel = parseInt(event.target.parentElement.querySelector('.max-level').textContent.trim());
+        if (price <= MONEY_VAL && minLevel < maxLevel) {
+            MONEY_VAL -= price;
+            updateMoney();
+            minLevel += 1;
+            event.target.parentElement.querySelector('.min-level').textContent = minLevel;
+            price = Math.trunc(price * 1.4)
+            event.target.textContent = price;
+
+            singingMoney[0] += 10;
+            singingMoney[1] = Math.trunc(singingMoney[1] * 1.1);
+
+            concertMoney[0] += 1000;
+            concertMoney[1] += 1000;
+
+            fameMultiplier += 50
+            concertMultiplier += 500
+
+            updateStats();
+        }
+
+        if (minLevel == maxLevel) {
+            event.target.textContent = 'MAX';
+            event.target.style.background = 'red'
+            event.target.style.cursor = 'not-allowed'
+        }
+    } else {
+        return;
+    }
 })
 
 statsBtn.addEventListener('click', () => {
