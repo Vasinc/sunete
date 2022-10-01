a048850f034ff1a0dd548e03c1350f52f18cc7a4
Vasinc <lein0.jade@gmail.com>
2022-10-01 22:37:47 +0300

    10 ( story book added )

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,29 @@
 const backdrop = document.querySelector('.backdrop');
 const burgerMenu = document.querySelector('.burger-menu');
 const links = document.querySelector('.links');
+const bookArrow = document.getElementById('bookArrow');
+const storyBook = document.getElementById('story-book');
+const pages = document.querySelector('.pages');
+
+const audio = new Audio('./assets/sounds/page flip.mp3')
+
+
+let arrowWasPressed = false;
+let isSoundPlaying = false;
+let scrollPosition = 0;
+let pagesPercentage = 0;
+
 
 function removeBackdrop() {
     backdrop.classList.remove('display-block');
     links.classList.remove('display-flex');
+    storyBook.classList.remove('display-block');
     document.body.style.overflow = 'visible';
+    if (arrowWasPressed) {
+        arrowWasPressed = false;
+        window.scrollTo(0, scrollPosition);
+        links.style.zIndex = 10000;
+    }
 }
 
 backdrop.addEventListener('click', removeBackdrop)
@@ -23,8 +41,36 @@ links.addEventListener('click', event => {
     }
 })
 
+bookArrow.addEventListener('click', () => {
+    backdrop.classList.add('display-block');
+    storyBook.classList.add('display-block');
+    document.body.style.overflow = 'hidden';
+    scrollPosition = window.scrollY;
+    arrowWasPressed = true;
+    backdrop.scrollIntoView();
+    links.style.zIndex = 9998;
+})
+
 addEventListener('resize', () => {
     if (innerWidth >= 1000 ) {
         removeBackdrop();
+    }
+})
+
+storyBook.addEventListener('click', event => {
+    if(event.target.className == 'right-arrow' && !isSoundPlaying) {
+        pagesPercentage -= 100;
+        pages.style.transform = `translate(${pagesPercentage}%)`
+        audio.play();
+        isSoundPlaying = true;
+        setTimeout(() => {
+            isSoundPlaying = false;
+        }, 1000);
+    } else if(event.target.className == 'left-arrow' && !isSoundPlaying) {
+        pagesPercentage +=100;
+        pages.style.transform = `translate(${pagesPercentage}%)`
+        audio.play();
+    } else {
+        return;
     }
 })
